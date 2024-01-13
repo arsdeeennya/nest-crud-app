81afaaf346bbe321138053428f9642fc19d31936
koborikan <koborikan@koborikannoMacBook-Pro.local>
2024-01-13 02:21:35 +0000

    update

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './item.model';
 import { ItemStatus } from './item-status.enum';
@@ -6,9 +6,15 @@ import { ItemStatus } from './item-status.enum';
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemService: ItemsService) {}
+
   @Get()
   findAll(): Item[] {
     return this.itemService.findAll();
+  }
+
+  @Get(':id') // /items/id
+  findById(@Param('id') id: string): Item {
+    return this.itemService.findById(id);
   }
 
   @Post()
@@ -26,5 +32,10 @@ export class ItemsController {
       status: ItemStatus.ON_SALE,
     };
     return this.itemService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemService.updateStatus(id);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
 
 @Injectable()
 export class ItemsService {
@@ -9,8 +10,18 @@ export class ItemsService {
     return this.items;
   }
 
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+
   create(item: Item): Item {
     this.items.push(item);
+    return item;
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = ItemStatus.SOLD_OUT;
     return item;
   }
 }
